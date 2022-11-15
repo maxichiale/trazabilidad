822f3a5fcfe8b5c7e125a00c05f3dd35d87696f7
Maximiliano <maxi.chiale@gmail.com>
2022-11-15 00:59:39 -0300

    add text as watermark

--- .next/static/webpack/webpack.3f7cff8487eb874c.hot-update.js
+++ .next/static/webpack/webpack.3f7cff8487eb874c.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1e4df505a502e49a"; }
+/******/ 	__webpack_require__.h = function() { return "e6d82f161645dd89"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
